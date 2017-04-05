d9cf906493fcefe5842923d720c68e5441c8d18d
Nikita Glukhov <n.gluhov@postgrespro.ru>
2017-04-05 22:53:12 +0300

    Add jsonpath sequences

--- src/include/utils/jsonpath.h
+++ src/include/utils/jsonpath.h
@@ -89,6 +89,7 @@ typedef enum JsonPathItemType
 	jpiLast,					/* LAST array subscript */
 	jpiStartsWith,				/* STARTS WITH predicate */
 	jpiLikeRegex,				/* LIKE_REGEX predicate */
+	jpiSequence,				/* sequence constructor: 'expr, ...' */
 } JsonPathItemType;
 
 /* XQuery regex mode flags for LIKE_REGEX predicate */
@@ -150,6 +151,12 @@ typedef struct JsonPathItem
 
 		struct
 		{
+			int32		nelems;
+			int32	   *elems;
+		}			sequence;
+
+		struct
+		{
 			char	   *data;	/* for bool, numeric and string/key */
 			int32		datalen;	/* filled only for string/key */
 		}			value;
@@ -177,6 +184,7 @@ extern bool jspGetBool(JsonPathItem *v);
 extern char *jspGetString(JsonPathItem *v, int32 *len);
 extern bool jspGetArraySubscript(JsonPathItem *v, JsonPathItem *from,
 					 JsonPathItem *to, int i);
+extern void jspGetSequenceElement(JsonPathItem *v, int i, JsonPathItem *elem);
 
 extern const char *jspOperationName(JsonPathItemType type);
 
@@ -229,6 +237,10 @@ struct JsonPathParseItem
 			uint32		patternlen;
 			uint32		flags;
 		}			like_regex;
+
+		struct {
+			List   *elems;
+		} sequence;
 
 		/* scalars */
 		Numeric numeric;
